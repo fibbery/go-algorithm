b71d2ae7bffac7525a1b88feeccf2cde8277cf8b
jiangnenghua <jiangnenghua1992@gmail.com>
2020-04-01 00:03:09 +0800

    快速排序2

--- sort/quick.go
+++ sort/quick.go
@@ -17,3 +17,34 @@ func quickSort(array []int) []int {
 	}
 	return append(quickSort(append(left, middle...)), quickSort(right)...)
 }
+
+
+// 第二种解法
+func sortArray(nums []int) []int {
+	quickSort2(nums, 0, len(nums) - 1)
+	return nums
+}
+
+func quickSort2(nums []int, left, right int) {
+	if left < right {
+		pos := partition(nums, left, right)
+		quickSort2(nums, left, pos - 1)
+		quickSort2(nums, pos + 1, right)
+	}
+}
+
+func partition(nums []int, left, right int)int {
+	pivot := left
+	//从基准值后一个序号开始处理
+	index := pivot + 1
+	for j := index ; j <= right; j ++ {
+		if nums[j] <= nums[pivot] {
+			nums[j], nums[index] = nums[index], nums[j]
+			index++
+		}
+	}
+	nums[pivot] , nums[index - 1] = nums[index - 1] , nums[pivot]
+	return index - 1
+}
+
+
